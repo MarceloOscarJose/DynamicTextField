a729bc5e8aa74c83a9fa81073b4566abf37f86d0
Marcelo Oscar Jose <marcelo.oscar.jose@gmail.com>
2017-01-28 22:25:44 -0300

    First Commit

---
+++ DynamicTextField/DynamicTextField.h
@@ -0,0 +1,19 @@
+//
+//  DynamicTextField.h
+//  DynamicTextField
+//
+//  Created by Marcelo Oscar José on 1/28/17.
+//  Copyright © 2017 Marcelo Oscar José. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicTextField.
+FOUNDATION_EXPORT double DynamicTextFieldVersionNumber;
+
+//! Project version string for DynamicTextField.
+FOUNDATION_EXPORT const unsigned char DynamicTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicTextField/PublicHeader.h>
+
+
